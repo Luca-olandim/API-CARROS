567eb7c3f683393ee90f70c2b6f32933ec17e67a
LUCA OLANDIM MELLO <luca_mello@estudante.sesisenai.org.br>
2024-09-20 08:48:22 -0300

    adicionando verbos http

--- database-postgres.js
+++ database-postgres.js
@@ -2,19 +2,36 @@ import { sql } from './db.js'
 import { randomUUID } from "crypto";
 
 export class DatabaseCarros {
-    async createCarro(carro) {
-        const id = randomUUID();
-        const marca = carro.marca;
-        const modelo = carro.modelo;
-        const placa = carro.placa;
+  async createCarro(carro) {
+    const id = randomUUID();
+    const marca = carro.marca;
+    const modelo = carro.modelo;
+    const placa = carro.placa;
 
-        await sql`insert into carros (id, marca, modelo, placa) 
-            values (${id}, ${marca}, ${modelo}, ${placa})`;
-    }
+    await sql`insert into carros (id, marca, modelo, placa) 
+      values (${id}, ${marca}, ${modelo}, ${placa})`;
+  }
 
-    async read(carros) {
-      carros = await sql`select * from carros`;
-    
-    }
+  async listCarros() {
+    const carros = await sql`select * from carros`;
+    return carros;
+  }
+  
+  async updateCarro(id, carro) {
+    const marca = carro.marca;
+    const modelo = carro.modelo;
+    const placa = carro.placa;
+
+    await sql`update users set 
+        marca = ${marca},
+        modelo = ${modelo},
+        placa = ${placa}
+        where id = ${id}
+    `;
+  }
+
+  async deleteCarro(id) {
+    await sql`delete from carros where id = ${id}`
+  }
   
 }
--- server.js
+++ server.js
@@ -20,18 +20,28 @@ server.post('/carros', async (request, reply) => {
     return 201
   });
 
-
-// READ
-server.get('/carros', async (request, reply) => {
-    const body = request.body;
-    await databasePostgres.get(body);
-    return 201
-  });
+// READE
+server.get('/carros', async () => {
+  const carros = await databasePostgres.listCarros();
+  return carros;
+});
 
 // UPDATE
+server.put('/carros/:id', async (request, reply) => {
+  const carroID = request.params.id;
+  const body = request.body;
+  await databasePostgres.updateCarro(carroID, body);
 
+  return reply.status(204).send();
+})
 
 // DELETE
+server.delete('/carros/:id', async (request, reply) => {
+  const carroID = request.params.id;
+  await databasePostgres.deleteCarro(carroID);
+
+  return reply.status(204).send();
+})
 
 
 server.listen({
